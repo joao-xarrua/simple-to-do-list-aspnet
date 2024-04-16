56527ef06900570070f1f818222e4526282039cd
root <root@xarras>
2024-04-16 02:51:13 -0300

    Crud with asp.net done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(); // adiciona controllers
+
+// cria a conexão e passa a utilizar AppDbContext como serviço podendo ser utilizado em outros arquivos
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers(); // mapeia controllers
 
 app.Run();
